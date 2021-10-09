664cf5e460b9e6a0ead5511595c7d5b52b80c125
EC2 Default User <ec2-user@ip-172-31-35-91.ec2.internal>
2021-10-09 05:20:38 +0000

    バージョンエラーを解決edit以外完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,38 @@
 class BooksController < ApplicationController
  def index
    @books = Book.all
+   @book = Book.new
  end
- 
+
  def create
-  @books = Book.new(book_params)
+  @book = Book.new(book_params)
+  @book.save
+  redirect_to books_path
  end
- 
+
+  def new
+  @book = Book.new
+  end
+
+ def show
+  @book =Book.find(params[:id])
+ end
+
+
+ def edit
+  @book =Book.find(params[:id])
+ end
+
+ def destroy
+  book=Book.find(params[:id])
+  book.destroy
+  redirect_to books_path
+
+ end
+
+private
+def book_params
+ params.require(:book).permit(:title,:body)
+end
+
 end
